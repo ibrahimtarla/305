11fe2535bb2c6bbf6811aee641fafa934a3fdebc
Ayhan Arslan MADEN <72784492+ayhanarslanmaden@users.noreply.github.com>
2020-12-06 18:32:12 +0300

    Update Face_Ayhan.js

--- anim/Face_Ayhan.js
+++ anim/Face_Ayhan.js
@@ -1,15 +1,14 @@
-class Color_Eyler extends Animator  {
+class Face_Ayhan extends Animator  {
     constructor() {
         super();
         this.author = 'Ayhan Arslan MADEN'
         this.desc = 'Smooth Face Change'
+        main.setAttribute("style", "background-color: white;");
         let x0 = "translateX(0)", x1 = "translateY(680px)", x2 = "translateX(540px)"
-        //let b = ["url('1.png')", "url('2.png')", "url('3.png')", "url('4.png')", "url('5.png')"]
-        let b = ["url('https://raw.githubusercontent.com/ayhanarslanmaden/AdvProgramming/main/images/1.png?token=ARLJU3HUKOGRFPX73FVE7G272YQFK')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/AdvProgramming/main/images/2.png?token=ARLJU3HUKOGRFPX73FVE7G272YQFK')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/AdvProgramming/main/images/3.png?token=ARLJU3HUKOGRFPX73FVE7G272YQFK')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/AdvProgramming/main/images/4.png?token=ARLJU3HUKOGRFPX73FVE7G272YQFK')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/AdvProgramming/main/images/5.png?token=ARLJU3HUKOGRFPX73FVE7G272YQFK')"]
+        let b = ["url('https://raw.githubusercontent.com/ayhanarslanmaden/image/main/1.png", "url('https://raw.githubusercontent.com/ayhanarslanmaden/image/main/2.png')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/image/main/3.png')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/image/main/4.png')", "url('https://raw.githubusercontent.com/ayhanarslanmaden/image/main/5.png')"]
         this.anim = { background: b, transform: [x0, x1, x2, (x1+x2), x0]  }
-        //this.anim = { transform: [x0, x1, x0] }
         this.elt = document.createElement('div')
-        this.elt.style = "width: 9.7%; height: 8%; position: relative;"
+        this.elt.style = "width: 9.7%; height: 8.5%; position: relative;"
         this.time = 12000
     }
     update() { }  //no code is needed here
