e1826b7ca708bedd16bbee59d510d8e92e220383
sametemret <43732189+sametemret@users.noreply.github.com>
2020-12-07 07:31:06 +0300

    Update SametEmre_Gif.js

--- anim/SametEmre_Gif.js
+++ anim/SametEmre_Gif.js
@@ -2,7 +2,7 @@ class SametEmre_Gif extends Animator {
     constructor() {
         super();
         this.author = 'Samet Emre Topaloğlu'
-        this.desc = 'Moving a basketballer gif with animation  .'
+        this.desc = 'Moving a basketballer gif with animation'
         let x0 = "translate(100px)", x1 = "translateY(500px)", r0 = "rotate(300deg)", r1 = "rotate(45deg)", x2 = "translateX(50px)"
         this.anim = { transform: [x0, r0, (x1+x2), x2, r1, x0] }
         this.elt = document.createElement('img')
